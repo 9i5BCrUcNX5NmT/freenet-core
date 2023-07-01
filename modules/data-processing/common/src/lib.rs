51cca8fb0c3520b7b4bdef9b301f55ae045545db
Ian Clarke <github@ian.33mail.com>
2023-07-01 12:59:05 -0500

    wip

--- modules/data-processing/common/src/lib.rs
+++ modules/data-processing/common/src/lib.rs
@@ -1,5 +1,10 @@
 pub mod dispatcher;
 pub mod segment;
-pub mod constants;
 
 pub use crate::segment::Segment;
+
+pub mod constants {
+    const MAXIMUM_JOB_ASSIGNMENT_TOKEN_AGE : std::time::Duration = std::time::Duration::from_secs(60 * 60 * 24 * 7);
+
+    const MAX_JOB_OUTPUT_RECORDS : usize = 5;
+}
--- modules/data-processing/contracts/job/src/structs/state.rs
+++ modules/data-processing/contracts/job/src/structs/state.rs
@@ -22,29 +22,26 @@ pub struct JobState {
     /// TODO: Should jobs be once-off or recurring like a cron job?
     pub authorizing_token : TokenAssignment,
 
-    /// The highest priority job_results are processed first.
-    pub job_results : (Vec<u8>, Vec<JobResult>),
+    /// The output (or outputs if different workers disagree), up to a maximum of
+    /// constants::MAX_JOB_OUTPUT_RECORDS.
+    pub outputs : Vec<JobOutput>,
+}
+
+/// 
+#[derive(Serialize, Deserialize)]
+pub struct JobOutput {
+    pub output : Vec<u8>,
+    pub worker_verifications : Vec<WorkerVerification>,
 }
 
 /// The output of a job and related metadata.
 #[derive(Serialize, Deserialize)]
-pub struct JobResult {
-    job_result_signature : Signature,
+pub struct WorkerVerification {
+    job_output_signature : Signature,
     worker : VerifyingKey,
     antiflood_token : TokenAssignment,
 }
 
-enum JobOutput {
-    Data(Vec<u8>),
-    Hash(Hash),
-}
-
-impl JobResult {
-    pub fn verify(&self) -> bool {
-       // self.worker.verify(self.job_result.as_slice(), &self.job_result_signature).is_ok()
-        todo!()
-    }
-}
 
 impl TryFrom<State<'_>> for JobState {
     type Error = ContractError;
@@ -58,51 +55,5 @@ impl TryFrom<JobState> for State<'static> {
     type Error = serde_json::Error;
     fn try_from(params: JobState) -> Result<Self, Self::Error> {
         serde_json::to_vec(&params).map(Into::into)
-    }
-}
-
-struct JobOutputVisitor;
-
-impl<'de> Visitor<'de> for JobOutputVisitor {
-    type Value = JobOutput;
-
-    fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
-        formatter.write_str("a byte vector or a hash string")
-    }
-
-    fn visit_bytes<E>(self, value: &[u8]) -> Result<Self::Value, E>
-    where
-        E: Error,
-    {
-        Ok(JobOutput::Data(value.to_vec()))
-    }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
-        let hash = Hash::from_hex(value).map_err(E::custom)?;
-        Ok(JobOutput::Hash(hash))
-    }
-}
-
-impl Serialize for JobOutput {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        match *self {
-            JobOutput::Data(ref vec) => vec.serialize(serializer),
-            JobOutput::Hash(ref hash) => serializer.serialize_bytes(hash.as_bytes()),
-        }
-    }
-}
-
-impl<'de> Deserialize<'de> for JobOutput {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_any(JobOutputVisitor)
     }
 }
